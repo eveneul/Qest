7cd4e56875360a820298eb6c6a02eabc7e7e74a5
eveneul <nebulass@naver.com>
2025-01-29 11:42:29 +0900

    검색 기능 만드는 중...

--- src/app/settings/projects/page.js
+++ src/app/settings/projects/page.js
@@ -5,6 +5,11 @@ import { useState, useEffect } from 'react';
 import ProjectsDataTable from './data-table';
 import { columns } from './columns';
 import QestPagination from '../../../components/Pagination';
+import { Label } from '@radix-ui/react-label';
+import { Input } from '@/components/ui/input';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
 
 export default function ProjectsSetting() {
 	const [projects, setProjects] = useState([]);
@@ -12,6 +17,11 @@ export default function ProjectsSetting() {
 	const [totalPage, setTotalPage] = useState(0);
 	const ROW_PER_PAGE = 10; // 한 페이지당 보여 줄 게시글 개수
 	const [loading, setLoading] = useState(false);
+	const form = useForm({
+		defaultValues: {
+			pjName: '',
+		},
+	});
 
 	useEffect(() => {
 		const fetchProjectList = async () => {
@@ -35,8 +45,53 @@ export default function ProjectsSetting() {
 		fetchProjectList();
 	}, [page]);
 
+	const onSearch = form.handleSubmit(async (values) => {
+		let query = supabaseClient.from('projects').select('*').order('id', { ascending: false });
+
+		if (values.pjName) {
+			query = query.ilike('name', `%${values.pjName}%`);
+		}
+
+		const { data, error } = await query;
+		if (error) {
+			console.log(error, 'error');
+		} else {
+			console.log(data, 'Data~~');
+		}
+	});
+
 	return (
 		<div>
+			<Form {...form}>
+				<form onSubmit={onSearch}>
+					<FormField
+						control={form.control}
+						name='pjName'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>프로젝트명</FormLabel>
+								<FormControl>
+									<Input placeholder='shadcn' {...field} />
+								</FormControl>
+							</FormItem>
+						)}></FormField>
+					<Button type='submit'>검색</Button>
+				</form>
+			</Form>
+			{/* <div className='flex flex-col gap-2'>
+				<div className='flex items-center gap-1.5'>
+					<Label htmlFor='pjName' className='text-sm font-medium text-black w-[120px]'>
+						프로젝트명
+					</Label>
+					<Input type='pjName' id='pjName' placeholder='프로젝트명을 입력해 주세요.' className='w-[200px]' />
+				</div>
+				<div className='flex items-center gap-1.5'>
+					<Label htmlFor='pjStatus' className='text-sm font-medium text-black w-[120px]'>
+						프로젝트 상태
+					</Label>
+					<Input type='pjStatus' id='pjStatus' placeholder='프로젝트명을 입력해 주세요.' className='w-[200px]' />
+				</div>
+			</div> */}
 			<ProjectsDataTable columns={columns} data={projects} />
 			<QestPagination currentPage={page} totalPages={totalPage} onChangePage={(newPage) => setPage(newPage)} />
 		</div>
